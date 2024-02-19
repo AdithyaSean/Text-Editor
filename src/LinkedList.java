8812834f7661896a8f49b4637b7128eef68880b1
AdithyaSean <adithyasean@outlook.com>
2024-02-19 13:46:14 +0530

    first commit

---
+++ src/LinkedList.java
@@ -0,0 +1,42 @@
+class Node {
+    Node prev;
+    String data;
+    Node next;
+
+    Node(String data) {
+        this.prev = null;
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedList {
+    static Node head;
+    static Node tail;
+
+    LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public static void insert(String data) {
+        Node node = new Node(data);
+
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            Node currentnode=head;
+            while(currentnode.next!=null){
+                currentnode = currentnode.next;
+            }
+            currentnode.next = node;
+            node.prev = currentnode;
+            tail = node;
+        }
+    }
+
+    public static void print() {
+        System.out.println(tail.data);
+    }
+}
---
+++ src/TextEditor.java
@@ -0,0 +1,197 @@
+import java.io.*;
+
+public class TextEditor extends javax.swing.JFrame {
+    public TextEditor() {
+        initComponents();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButtonUndo = new javax.swing.JButton();
+        jButtonRedo = new javax.swing.JButton();
+        jButtonFind = new javax.swing.JButton();
+        jButtonReplace = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea = new javax.swing.JTextArea();
+        txtField = new javax.swing.JTextField();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu = new javax.swing.JMenu();
+        openItem = new javax.swing.JMenuItem();
+        saveItem = new javax.swing.JMenuItem();
+        exitItem = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButtonUndo.setText("Undo");
+
+        jButtonRedo.setText("Redo");
+
+        jButtonFind.setText("Find");
+
+        jButtonReplace.setText("Replace");
+
+        jTextArea.setColumns(20);
+        jTextArea.setRows(5);
+        jTextArea.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jTextAreaKeyTyped(evt);
+            }
+        });
+        jScrollPane2.setViewportView(jTextArea);
+
+        txtField.setText("Find and Replace");
+        txtField.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtFieldFocusGained(evt);
+            }
+        });
+
+        jMenu.setText("File");
+
+        openItem.setText("Open");
+        openItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                openItemActionPerformed(evt);
+            }
+        });
+        jMenu.add(openItem);
+
+        saveItem.setText("Save");
+        jMenu.add(saveItem);
+
+        exitItem.setText("Exit");
+        exitItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exitItemActionPerformed(evt);
+            }
+        });
+        jMenu.add(exitItem);
+
+        jMenuBar1.add(jMenu);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButtonUndo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButtonRedo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtField, javax.swing.GroupLayout.DEFAULT_SIZE, 188, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButtonFind)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButtonReplace)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonUndo)
+                    .addComponent(jButtonRedo)
+                    .addComponent(jButtonFind)
+                    .addComponent(jButtonReplace)
+                    .addComponent(txtField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 256, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void exitItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitItemActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_exitItemActionPerformed
+
+    private void jTextAreaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextAreaKeyTyped
+        LinkedList.insert(jTextArea.getText());
+        LinkedList.print();
+    }//GEN-LAST:event_jTextAreaKeyTyped
+
+    private void openItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_openItemActionPerformed
+        String filePath = System.getProperty("user.home"); // file path
+        
+        
+        try {
+            File file = new File(filePath);
+            String line;
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            while ((line = reader.readLine()) != null) {
+                jTextArea.append(line + "\n"); // Append each line to the TextArea
+            }
+            reader.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_openItemActionPerformed
+
+    private void txtFieldFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtFieldFocusGained
+        if(txtField.getText().equals("Find and Replace")){
+            txtField.setText("");
+        }
+       
+    }//GEN-LAST:event_txtFieldFocusGained
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TextEditor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TextEditor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TextEditor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TextEditor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TextEditor().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem exitItem;
+    private javax.swing.JButton jButtonFind;
+    private javax.swing.JButton jButtonRedo;
+    private javax.swing.JButton jButtonReplace;
+    private javax.swing.JButton jButtonUndo;
+    private javax.swing.JMenu jMenu;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea;
+    private javax.swing.JMenuItem openItem;
+    private javax.swing.JMenuItem saveItem;
+    private javax.swing.JTextField txtField;
+    // End of variables declaration//GEN-END:variables
+}
