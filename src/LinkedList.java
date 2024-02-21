e68b7acfdcd8fb36e32169a2bdac48c5dd5d8aea
Adithya Ekanayaka <130180324+AdithyaSean@users.noreply.github.com>
2024-02-21 20:52:45 +0530

    Add files via upload

---
+++ src/LinkedList.java
@@ -0,0 +1,33 @@
+class Node {
+    Node prev;
+    String data;
+    Node next;
+
+    Node(String data) {
+        this.prev = null;
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedList {
+    static Node head;
+    static Node tail;
+
+    public static void insert(String data) {
+        Node node = new Node(data);
+
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+    }
+
+    public static void print() {
+        System.out.println(tail.data);
+    }
+}
