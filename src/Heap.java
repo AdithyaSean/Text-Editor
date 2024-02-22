c54a6c6cbcfbfd4e33920a0eabc52fef6cb18ce3
AdithyaSean <adithyasean@outlook.com>
2024-02-22 15:04:22 +0530

    fix netbeans build issue fix

--- src/Heap.java
+++ src/Heap.java
@@ -9,9 +9,7 @@ public class Heap {
         Heap = new int[this.maxsize];
     }
 
-    private int parent(int pos) { 
-        return (pos - 1) / 2;
-    }
+    
 
     private int leftChild(int pos) {
         return (2 * pos) + 1;
